d1250fda2bb6ef431a2460de82902207bd3422f6
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-12-16 06:18:56 +0000

    greeting

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+const [value,setValue] = useState("");
+
+function modifyValue(value){
+  setValue("Hello "+value+"!")
+}
   return (
     <div>
-        {/* Do not remove the main div */}
+       <p>Enter your name:</p>
+       <input type="text" onChange={(e)=>{
+        modifyValue(e.target.value)
+       }}></input>
+
+      <p>{value}</p>
+
     </div>
   )
 }
